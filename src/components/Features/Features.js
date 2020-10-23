5e841677e26cf0705b021b301ebc2463049f0fa1
vit_ru <vctor_r@mail.ru>
2020-10-23 12:56:36 +0300

    day 2 homework part 1

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -3,10 +3,17 @@ import RelaxWrapper from 'react-rellax-wrapper';
 
 import './features.css';
 
-const Features = () => (
+const video = {
+    'Falcon 1': 'falcon-1',
+    'Falcon 9': 'falcon-9',
+    'Falcon Heavy': 'falcon-heavy',
+    other: 'starship'
+};
+
+const Features = ({ name, height, diameter, mass,  payload_weights, description }) => (
     <section className="features">
         <h2 className="features-title">
-            Falcon 1 <br/>Overview
+            {name} <br/>Overview
         </h2>
         <div className="overview">
 
@@ -17,25 +24,26 @@ const Features = () => (
                 <thead>
                     <tr>
                         <td className="table-column">HEIGHT</td>
-                        <td className="table-column">22.25 m / 73 ft</td>
+                        <td className="table-column">{height ? height.meters : ""} m / {height ? height.feet : ""} ft</td>
                     </tr>
                     <tr>
                         <td className="table-column">DIAMETER</td>
-                        <td className="table-column">1.68 m / 5.5 ft</td>
+                        <td className="table-column">{diameter ? diameter.meters : ""} m / {diameter ? diameter.feet : ""} ft</td>
                     </tr>
                     <tr>
                         <td className="table-column">MASS</td>
-                        <td className="table-column">30,146 kg / 66,460 lb</td>
+                        <td className="table-column">{mass ? mass.kg : ""} kg / {mass ? mass.lb : ""} lb</td>
                     </tr>
                     <tr>
                         <td className="table-column">PAYLOAD TO LEO</td>
-                        <td className="table-column">450 kg / 992 lb</td>
+                        <td className="table-column">{payload_weights ? payload_weights[0].kg : ""} kg / {payload_weights ? payload_weights[0].lb : ""} lb</td>
                     </tr>
                 </thead>
             </table>
             <RelaxWrapper speed={14}>
                 <img
-                        src="img/falcon-1.png"
+                        src={`img/${video.hasOwnProperty(name) ? 
+                            video[name] : video.other}.png`}
                         alt="rocket"
                         className="rocket"
                 />
@@ -43,7 +51,7 @@ const Features = () => (
             <article>
                 <h3 className="features-subtitle">DESCRIPTION</h3>
                 <p className="features-text">
-                    The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.
+                    {description}
                 </p>
             </article>
         </div>
